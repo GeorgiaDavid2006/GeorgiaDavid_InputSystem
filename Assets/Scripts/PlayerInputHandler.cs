534e9d7dc4681e7a57a9fb7f97bb876ea06325f1
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2026-01-08 15:39:58 -0400

    Kick and movement Input Actions

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody ballRb;
+    public Vector2 moveInput;
+
+    private Vector3 direction;
+    private float kickForce = 5f;
+
+    public void OnKick(InputAction.CallbackContext context)
     {
-        
+        if (context.started)
+        {
+            Debug.Log("Kick started");
+            ballRb.AddForce(direction * kickForce, ForceMode.Impulse);
+        }
+
+        if (context.canceled)
+        {
+            Debug.Log("Kick done");
+        }
+    }
+    public void Moving(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
